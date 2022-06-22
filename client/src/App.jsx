7108e265bd39b9b37dfb8f0543e7e88e1c8a585a
Sam <samueljoh329@gmail.com>
2022-06-22 13:34:10 -0500

    implemented functionality for searching causes by city, state, or both

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   const [causes, setCauses] = useState([]);
   const [searchName, setSearchName] = useState("");
   const [searchCity, setSearchCity] = useState("");
+  const [searchState, setSearchState] = useState("");
 
   useEffect(() => {
     axios
@@ -17,15 +18,42 @@ function App() {
   const handleSearchName = () => {
     axios
       .get(
-        `https://api.givebacks.com/services/core/causes/search?search[name][value]=${search}`
+        `https://api.givebacks.com/services/core/causes/search?search[name][value]=${searchName}`
       )
       .then((res) => setCauses(res.data.causes));
   };
 
-  console.log(causes);
+  const handleSearchLocation = () => {
+    if (!!searchCity && !!searchState === false) {
+      //fetch for city
+      axios
+        .get(
+          `https://api.givebacks.com/services/core/causes/search?search[city][value]=${searchCity}`
+        )
+        .then((res) => setCauses(res.data.causes));
+    } else if (!!searchState && !!searchCity === false) {
+      //fetch for state
+      axios
+        .get(
+          `https://api.givebacks.com/services/core/causes/search?search[state][value]=${searchState}`
+        )
+        .then((res) => setCauses(res.data.causes));
+    } else {
+      //fetch for both
+      console.log("hit");
+      axios
+        .get(
+          `https://api.givebacks.com/services/core/causes/search?join=AND&search[city][value]=${searchCity}&search[state][value]=${searchState}`
+        )
+        .then((res) => setCauses(res.data.causes));
+    }
+  };
+
+  // console.log(!!searchState && !!searchCity);
   return (
     <div className="App">
       <header>Givebacks</header>
+
       <div className="search">
         <div className="search__name">
           <p>Search cause by name:</p>
@@ -44,32 +72,46 @@ function App() {
             Search
           </button>
         </div>
+
         <div className="search__location">
           <p>Search cause by location:</p>
-          <label htmlFor="city-search"></label>
+          <label htmlFor="city-search">City: </label>
           <input
             type="text"
             value={searchCity}
             onChange={(e) => setSearchCity(e.target.value)}
-            placeholder="Enter Cause Here"
+            placeholder="Enter City Here"
+          />
+
+          <label htmlFor="state-search">State: </label>
+          <input
+            type="text"
+            value={searchState}
+            onChange={(e) => setSearchState(e.target.value)}
+            placeholder="Enter State Here"
           />
           <button
             onClick={() => {
-              handleSearchName();
+              handleSearchLocation();
             }}
           >
             Search
           </button>
         </div>
       </div>
+
       <div className="causes">
-        {causes.map((cause) => {
-          return (
-            <div className="single-cause">
-              <p>{cause.name}</p>
-            </div>
-          );
-        })}
+        {causes.length < 1 ? (
+          <h1>No availble causes!</h1>
+        ) : (
+          causes.map((cause) => {
+            return (
+              <div className="single-cause">
+                <p>{cause.name}</p>
+              </div>
+            );
+          })
+        )}
       </div>
     </div>
   );
