b587a0a312c4ad7cddec5025f1dd7d7fdb2aa0b6
Sam <samueljoh329@gmail.com>
2022-06-22 16:17:19 -0500

    added TailwindCSS and styled title, search bar, and causes cards

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -65,106 +65,96 @@ function App() {
     }
   };
 
-  // const handleSearchName = () => {
-  //   axios
-  //     .get(
-  //       `https://api.givebacks.com/services/core/causes/search?search[name][value]=${searchName}`
-  //     )
-  //     .then((res) => {
-  //       setCauses(res.data.causes);
-  //       if (res.data.meta.has_more) {
-  //         setCausesLeft(true);
-  //       } else {
-  //         setCausesLeft(false);
-  //       }
-  //     });
-  // };
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      const baseURL = "https://api.givebacks.com/services/core/causes/search?";
+      let nameURL = "";
+      let cityURL = "";
+      let stateURL = "";
+      let count = 0;
 
-  // const handleSearchLocation = () => {
-  //   if (!!searchCity && !!searchState === false) {
-  //     //fetch for city
-  //     axios
-  //       .get(
-  //         `https://api.givebacks.com/services/core/causes/search?search[city][value]=${searchCity}`
-  //       )
-  //       .then((res) => {
-  //         setCauses(res.data.causes);
-  //         if (res.data.meta.has_more) {
-  //           setCausesLeft(true);
-  //         } else {
-  //           setCausesLeft(false);
-  //         }
-  //       });
-  //   } else if (!!searchState && !!searchCity === false) {
-  //     //fetch for state
-  //     axios
-  //       .get(
-  //         `https://api.givebacks.com/services/core/causes/search?search[state][value]=${searchState}`
-  //       )
-  //       .then((res) => {
-  //         setCauses(res.data.causes);
-  //         if (res.data.meta.has_more) {
-  //           setCausesLeft(true);
-  //         } else {
-  //           setCausesLeft(false);
-  //         }
-  //       });
-  //   } else {
-  //     //fetch for both
-  //     axios
-  //       .get(
-  //         `https://api.givebacks.com/services/core/causes/search?join=AND&search[city][value]=${searchCity}&search[state][value]=${searchState}`
-  //       )
-  //       .then((res) => {
-  //         setCauses(res.data.causes);
-  //         if (res.data.meta.has_more) {
-  //           setCausesLeft(true);
-  //         } else {
-  //           setCausesLeft(false);
-  //         }
-  //       });
-  //   }
-  // };
+      if (searchName) {
+        nameURL = `search[name][value]=${searchName}`;
+        count++;
+      }
+      if (searchCity) {
+        cityURL = `search[city][value]=${searchCity}`;
+        count++;
+      }
+      if (searchState) {
+        stateURL = `search[state][value]=${searchState}`;
+        count++;
+      }
+
+      if (count < 2) {
+        axios.get(`${baseURL}${nameURL}${cityURL}${stateURL}`).then((res) => {
+          setCauses(res.data.causes);
+          if (res.data.meta.has_more) {
+            setCausesLeft(true);
+          } else {
+            setCausesLeft(false);
+          }
+        });
+      } else {
+        axios
+          .get(`${baseURL}join=AND&${nameURL}&${cityURL}&${stateURL}`)
+          .then((res) => {
+            setCauses(res.data.causes);
+            if (res.data.meta.has_more) {
+              setCausesLeft(true);
+            } else {
+              setCausesLeft(false);
+            }
+          });
+      }
+    }
+  };
 
   return (
     <div className="App">
-      <header>
-        <h1>Givebacks Cause Search</h1>
-      </header>
-
-      <div className="search">
-        <div className="search__bar">
-          <label htmlFor="name-search">Cause Name: </label>
-          <input
-            type="text"
-            value={searchName}
-            onChange={(e) => setSearchName(e.target.value)}
-            placeholder="Enter Cause Here"
-          />
-          <label htmlFor="city-search">City: </label>
-          <input
-            type="text"
-            value={searchCity}
-            onChange={(e) => setSearchCity(e.target.value)}
-            placeholder="Enter City Here"
-          />
-
-          <label htmlFor="state-search">State: </label>
-          <input
-            type="text"
-            value={searchState}
-            onChange={(e) => setSearchState(e.target.value)}
-            placeholder="Enter State Here"
-          />
+      <header className="p-8 flex justify-between items-center">
+        <h1 className="text-orange-400 text-4xl font-medium">givebacks</h1>{" "}
+        <div className="search flex items-center bg-gray-50 border border-gray-300 rounded-lg divide-x">
+          <div className="search-name">
+            <input
+              type="text"
+              value={searchName}
+              onChange={(e) => setSearchName(e.target.value)}
+              placeholder="Cause Name"
+              className="rounded-l-lg bg-gray-50 text-gray-900 text-sm  focus:ring-orange-400 focus:border-orange-400 block w-full p-2.5 "
+              onKeyDown={handleKeyPress}
+            />
+          </div>
+          <div className="search-city">
+            <input
+              type="text"
+              value={searchCity}
+              onChange={(e) => setSearchCity(e.target.value)}
+              placeholder="City"
+              className="bg-gray-50 text-gray-900 text-sm  focus:ring-orange-400 focus:border-orange-400 block w-full p-2.5"
+              onKeyDown={handleKeyPress}
+            />
+          </div>
+          <div className="search-state">
+            <input
+              type="text"
+              value={searchState}
+              onChange={(e) => setSearchState(e.target.value)}
+              placeholder="State"
+              className=" bg-gray-50 text-gray-900 text-sm  focus:ring-orange-400 focus:border-orange-400 block w-full p-2.5 "
+              onKeyDown={handleKeyPress}
+            />
+          </div>
           <button
             onClick={() => {
               handleSearch();
             }}
+            className=" mx-2 h-8 w-16 text-orange-400 hover:text-white !border border-orange-400 hover:bg-orange-500 focus:ring-2 focus:outline-none focus:ring-orange-300  rounded-lg text-xs text-center "
           >
             Search
           </button>
         </div>
-      </div>
+      </header>
 
       <div className="causes">
         {causes.length < 1 ? (
@@ -172,17 +162,45 @@ function App() {
         ) : (
           <>
             {causesLeft ? (
-              <h2>{`Over ${causes.length} results:`}</h2>
+              <h2 className="py-4 text-2xl">{`Over ${causes.length} results:`}</h2>
             ) : (
               <h2>{`${causes.length} results:`}</h2>
             )}
-            {causes.map((cause) => {
-              return (
-                <div className="single-cause" key={cause.id}>
-                  <p>{cause.name}</p>
-                </div>
-              );
-            })}
+            <div className="cause-cards grid gap-8 grid-cols-4 px-4 ">
+              {causes.map((cause) => {
+                return (
+                  <div
+                    className="single-cause p-4 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md flex flex-col justify-between gap-4"
+                    key={cause.id}
+                  >
+                    <div>
+                      <p className="text-xl">{cause.name}</p>
+                    </div>
+                    <div>
+                      <p className="text-md">{cause.address}</p>
+                    </div>
+                    <div>
+                      <a href={cause.details_url} target="_blank">
+                        <button
+                          type="button"
+                          class="focus:outline-none text-white bg-orange-400 hover:bg-orange-500 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-orange-900"
+                        >
+                          Details
+                        </button>
+                      </a>
+                      <a href={cause.join_url} target="_blank">
+                        <button
+                          type="button"
+                          class="focus:outline-none text-white bg-orange-400 hover:bg-orange-500 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-orange-900"
+                        >
+                          Support
+                        </button>
+                      </a>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
           </>
         )}
       </div>
